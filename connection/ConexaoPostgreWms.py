7fc1301e0709e76217cd106a35b2cf39d952e265
luis-fe <luisengp@gmail.com>
2024-06-11 17:46:30 -0300

    correcoes

--- connection/ConexaoPostgreWms.py
+++ connection/ConexaoPostgreWms.py
@@ -1,12 +1,14 @@
 '''Arquivo para chamar a conexao de banco com o postgre'''
+import os
+
 import psycopg2
 from sqlalchemy import create_engine
 
-def conexaoEngine(host, bancoDados):
-    db_name = bancoDados
+def conexaoEngine():
+    db_name = os.getenv('POSTGRES_DB')
     db_user = "postgres"
     db_password = "Master100"
-    db_host = host
+    db_host = os.getenv('DATABASE_HOST')
     portbanco = "5432"
 
     connection_string = f"postgresql://{db_user}:{db_password}@{db_host}:{portbanco}/{db_name}"
