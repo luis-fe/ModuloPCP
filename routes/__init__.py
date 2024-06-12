03c3f43888d60810726ffb324dabf3fa94958fd3
luis-fe <luisengp@gmail.com>
2024-06-12 15:27:21 -0300

    atualizacao Projeto3

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,5 +4,7 @@ routes_blueprint = Blueprint('routes', __name__)
 
 
 from .Planejamento.plano_route import planoPCP_routes
+from .MonitorPedidos.monitor_route import MonitorPedidos_routes
 
 routes_blueprint.register_blueprint(planoPCP_routes)
+routes_blueprint.register_blueprint(MonitorPedidos_routes)
