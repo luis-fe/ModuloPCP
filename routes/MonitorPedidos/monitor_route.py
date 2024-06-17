9681614ac70e27b5dc1326b2255727f45afba008
luis-fe <luisengp@gmail.com>
2024-06-17 15:57:41 -0300

    criando o simulador de ops

--- routes/MonitorPedidos/monitor_route.py
+++ routes/MonitorPedidos/monitor_route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from functools import wraps
-from models.MonitorPedidos import monitor
+from models.MonitorPedidos import monitor, MonitorSimulacaoEncerrOP
 
 MonitorPedidos_routes = Blueprint('MonitorPedidos_routes', __name__)
 
@@ -29,6 +29,34 @@ def get_MonitorPedidos():
 
     #controle.InserindoStatus(rotina, ip, datainicio)
     dados = monitor.API(empresa, iniVenda, finalVenda, tiponota,'rotina', 'ip', 'datainicio',parametroClassificacao, tipoData, arrayRepres_excluir, arrayRepre_Incluir)
+    #controle.salvarStatus(rotina, ip, datainicio)
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    return jsonify(OP_data)
+
+@MonitorPedidos_routes.route('/pcp/api/monitorPreFaturamentoSimulaOP', methods=['GET'])
+@token_required
+def get_monitorPreFaturamentoSimulaOP():
+    empresa = request.args.get('empresa')
+    iniVenda = request.args.get('iniVenda','-')
+    finalVenda = request.args.get('finalVenda')
+    tiponota = request.args.get('tiponota')
+    parametroClassificacao = request.args.get('parametroClassificacao', 'DataPrevisao')  # Faturamento ou DataPrevisao
+    tipoData = request.args.get('tipoData','DataEmissao') #DataEmissao x DataPrevOri
+    arrayRepres_excluir = request.args.get('arrayRepres_excluir','')
+    arrayRepre_Incluir = request.args.get('arrayRepre_Incluir','')
+    ops = request.args.get('ops','')
+
+    #controle.InserindoStatus(rotina, ip, datainicio)
+    dados = MonitorSimulacaoEncerrOP.API(empresa, iniVenda, finalVenda, tiponota,'rotina', 'ip', 'datainicio',parametroClassificacao, tipoData, arrayRepres_excluir, arrayRepre_Incluir,ops)
     #controle.salvarStatus(rotina, ip, datainicio)
 
     # Obtém os nomes das colunas
