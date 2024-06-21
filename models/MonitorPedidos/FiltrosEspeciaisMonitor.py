f364895128a1700ab16f2b3c3f1415af2813f06b
luis-fe <luisengp@gmail.com>
2024-06-21 16:29:08 -0300

    adicionando filtro notas

--- models/MonitorPedidos/FiltrosEspeciaisMonitor.py
+++ models/MonitorPedidos/FiltrosEspeciaisMonitor.py
@@ -1,14 +1,32 @@
 import pandas as pd
-from connection import ConexaoPostgreWms
+from connection import ConexaoPostgreWms, ConexaoBanco
 
 
 
 
 def FiltroTipoNotas():
     SQL = """
-    select TNM."cod_desTipoNota"  from "PCP".pcp."tipoNotaMonitor" tnm 
+    select TNM."cod_desTipoNota" as descricao  from "PCP".pcp."tipoNotaMonitor" tnm 
     """
     conn = ConexaoPostgreWms.conexaoEngine()
     consulta = pd.read_sql(SQL,conn)
+
+    return consulta
+
+def FiltroConceitoClientes():
+
+    sql = """
+    SELECT c.descricao  FROM fat.ConceitoCred c
+    WHERE c.codEmpresa =1 
+    """
+
+    with ConexaoBanco.ConexaoInternoMPL() as conn:
+        with conn.cursor() as cursor_csw:
+            # Executa a primeira consulta e armazena os resultados
+            cursor_csw.execute(sql)
+            colunas = [desc[0] for desc in cursor_csw.description]
+            rows = cursor_csw.fetchall()
+            consulta = pd.DataFrame(rows, columns=colunas)
+            del rows, colunas
 
     return consulta
--- routes/MonitorPedidos/FiltrosEspeciaisMonitor.py
+++ routes/MonitorPedidos/FiltrosEspeciaisMonitor.py
@@ -32,3 +32,23 @@ def get_FiltroTipoNotas():
         OP_data.append(op_dict)
     del dados
     return jsonify(OP_data)
+
+
+@FiltrosEspeciaisMonitor_routes.route('/pcp/api/FiltroConceitoClientes', methods=['GET'])
+@token_required
+def get_FiltroConceitoClientes():
+
+    dados = FiltrosEspeciaisMonitor.FiltroConceitoClientes()
+    #controle.salvarStatus(rotina, ip, datainicio)
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
