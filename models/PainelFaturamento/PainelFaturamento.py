57051cc3840e73e258cd1482663671d2cf9cc499
luis-fe <luisengp@gmail.com>
2024-07-02 11:27:10 -0300

    atende em pcs

--- models/PainelFaturamento/PainelFaturamento.py
+++ models/PainelFaturamento/PainelFaturamento.py
@@ -648,7 +648,7 @@ def Backup(ano, empresa):
 
     dataInicio = ano + '-01-01'
     backupAno = True
-    if mesFinal in ['11','4','06','09']:
+    if mesFinal in ['11','4','6','09']:
         dataFim = ano + '-'+mesFinal+'-30'
     elif mesFinal in ['01']:
         backupAno = False
