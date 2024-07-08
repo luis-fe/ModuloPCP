ddb2c19a3aaa36ad2243e7eb0fe3a380f196b1ad
luis-fe <luisengp@gmail.com>
2024-07-08 08:38:51 -0300

    atualizacao Monitor

--- routes/MonitorPedidos/monitor_route.py
+++ routes/MonitorPedidos/monitor_route.py
@@ -26,9 +26,10 @@ def get_MonitorPedidos():
     tipoData = request.args.get('tipoData','DataEmissao') #DataEmissao x DataPrevOri
     arrayRepres_excluir = request.args.get('arrayRepres_excluir','')
     arrayRepre_Incluir = request.args.get('arrayRepre_Incluir','')
+    nomeCliente = request.args.get('nomeCliente','')
 
     #controle.InserindoStatus(rotina, ip, datainicio)
-    dados = monitor.API(empresa, iniVenda, finalVenda, tiponota,'rotina', 'ip', 'datainicio',parametroClassificacao, tipoData, arrayRepres_excluir, arrayRepre_Incluir)
+    dados = monitor.API(empresa, iniVenda, finalVenda, tiponota,'rotina', 'ip', 'datainicio',parametroClassificacao, tipoData, arrayRepres_excluir, arrayRepre_Incluir, nomeCliente)
     #controle.salvarStatus(rotina, ip, datainicio)
 
     # Obtém os nomes das colunas
@@ -41,6 +42,61 @@ def get_MonitorPedidos():
             op_dict[column_name] = row[column_name]
         OP_data.append(op_dict)
     return jsonify(OP_data)
+
+
+@MonitorPedidos_routes.route('/pcp/api/monitorPreFaturamento', methods=['POST'])
+@token_required
+def POST_MonitorPedidos():
+    data = request.get_json()
+    empresa = data.get('empresa', '-')
+
+    # Parametros obrigatorios no POST
+    iniVenda = data.get('iniVenda','-')
+    finalVenda = data.get('finalVenda')
+    parametroClassificacao = data.get('parametroClassificacao', 'DataPrevisao')  # Faturamento ou DataPrevisao
+    tipoData = data.args.get('tipoData','DataEmissao') #DataEmissao x DataPrevOri
+
+    # Parametros NAO OBRIGATORIOS NO POST (ESPECIAIS)
+    # Array de tipo de nota
+    tiponota = data.get('tiponota')
+
+    # Array excluir codigo representante
+    ArrayCodRepresExcluir = data.get('ArrayCodRepresExcluir','')
+
+    # Array codrepresentante
+    ArrayCodRepres = data.get('ArrayCodRepres','')
+
+    # ARRAY NOME CLIENTE
+    ArrayNomeCliente = data.get('ArrayNomeCliente','')
+
+    #ARRAY CODIGO CLIENTE
+    ArrayCodigoCliente= data.get('ArrayCodigoCliente','')
+
+    #ARRAY CONCEITO CLIENTE
+    ArrayConceitoCliente= data.get('ArrayConceitoCliente','')
+
+    #ARRA REGIAO
+    ArrayRegiao= data.get('ArrayRegiao','')
+
+
+    #controle.InserindoStatus(rotina, ip, datainicio)
+    dados = monitor.API(empresa, iniVenda, finalVenda, tiponota,'rotina', 'ip', 'datainicio',parametroClassificacao, tipoData, ArrayCodRepresExcluir, ArrayCodRepres, ArrayNomeCliente)
+    #controle.salvarStatus(rotina, ip, datainicio)
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    return jsonify(OP_data)
+
+
+
+
 
 @MonitorPedidos_routes.route('/pcp/api/monitorPreFaturamentoSimulaOP', methods=['GET'])
 @token_required
