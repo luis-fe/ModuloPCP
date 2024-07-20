d7a6c5c3283313237eccebfc8cc6d5c80259a5ff
luis-fe <luisengp@gmail.com>
2024-07-20 11:35:16 -0300

    atualizando o controle de golas2

--- models/Planejamento/plano.py
+++ models/Planejamento/plano.py
@@ -29,21 +29,29 @@ def ObeterPlanos():
     lotes['01- Codigo Plano'] = lotes['01- Codigo Plano'].astype(str)
 
     merged = pd.merge(planos, lotes, on='01- Codigo Plano', how='left')
-    grouped = merged.groupby(['01- Codigo Plano', '02- Descricao do Plano']).agg({
+
+    # Agrupa mantendo todas as colunas do DataFrame planos
+    grouped = merged.groupby(['01- Codigo Plano', '02- Descricao do Plano', '03- Inicio Venda', '04- Final Venda',
+                              '05- Inicio Faturamento', '06- Final Faturamento', '07- Usuario Gerador', '08- Data Geracao']).agg({
         'lote': lambda x: ','.join(x.dropna().astype(str)),
         'nomelote': lambda x: ','.join(x.dropna().astype(str))
     }).reset_index()
 
-    result = {}
+    result = []
     for index, row in grouped.iterrows():
-        plano = row['01- Codigo Plano']
-        descricao = row['02- Descricao do Plano']
-        lotes = row['lote']
-        nomelote = row['nomelote']
-        result[plano] = {
-            'descricao': descricao,
-            'lotes': lotes,
-            'nomelote': nomelote
+        entry = {
+            '01- Codigo Plano': row['01- Codigo Plano'],
+            '02- Descricao do Plano': row['02- Descricao do Plano'],
+            '03- Inicio Venda': row['03- Inicio Venda'],
+            '04- Final Venda': row['04- Final Venda'],
+            '05- Inicio Faturamento': row['05- Inicio Faturamento'],
+            '06- Final Faturamento': row['06- Final Faturamento'],
+            '07- Usuario Gerador': row['07- Usuario Gerador'],
+            '08- Data Geracao': row['08- Data Geracao'],
+            'lotes': row['lote'],
+            'nomelote': row['nomelote']
         }
+        result.append(entry)
 
     return result
+
