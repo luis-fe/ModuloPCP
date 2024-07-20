8bbad2087c4b1b1511257008fa6f1e152aa83f3b
luis-fe <luisengp@gmail.com>
2024-07-20 19:54:07 -0300

    testando o replace dos itens e medindo o tempo2

--- models/Planejamento/itemsPA_Csw.py
+++ models/Planejamento/itemsPA_Csw.py
@@ -4,12 +4,22 @@ from connection import ConexaoPostgreWms, ConexaoBanco
 
 
 def RecarregarItens():
+    # passo 1: obter ultimo registro do sql itens_csw
+    sql = """
+    select max(codigo::int) as maximo from "PCP".pcp.itens_csw
+    """
+
+    conn = ConexaoPostgreWms.conexaoEngine()
+
+    sqlMax = pd.read_sql(sql,conn)
+    maximo = sqlMax['maximo'][0]
+
+
     sqlCSWItens = """
 SELECT i.codigo , i.nome , i.unidadeMedida, i2.codItemPai, i2.codSortimento , i2.codSeqTamanho  FROM cgi.Item i
 JOIN Cgi.Item2 i2 on i2.coditem = i.codigo and i2.Empresa = 1
 WHERE i.unidadeMedida in ('PC','KIT') and (i2.coditem like '1%' or i2.coditem like '2%'or i2.coditem like '3%'or i2.coditem like '5%'or i2.coditem like '6%' )
-and i2.codItemPai > 0
-  """
+and i2.codItemPai > 0 and i.codigo > """+str(maximo)
 
     with ConexaoBanco.Conexao2() as conn:
         with conn.cursor() as cursor:
@@ -24,6 +34,6 @@ and i2.codItemPai > 0
 
 
     #Implantando no banco de dados do Pcp
-    ConexaoPostgreWms.Funcao_InserirOFF(itens, itens['codigo'].size, 'itens_csw', 'replace')
+    ConexaoPostgreWms.Funcao_InserirOFF(itens, itens['codigo'].size, 'itens_csw', 'append')
 
     return itens
