2abd6540ca65a4f5c82894b3f4e68306be39fbd9
luis-fe <luisengp@gmail.com>
2024-07-23 17:27:36 -0300

    teste2

--- models/Planejamento/acomp_meta_plano.py
+++ models/Planejamento/acomp_meta_plano.py
@@ -53,8 +53,8 @@ def MetasFase(plano, arrayCodLoteCsw):
     sqlMetas['carga'].fillna(0,inplace=True)
 
     sqlMetas['estoque-saldoAnt'] = sqlMetas['estoqueAtual'] - sqlMetas['saldo']
-    sqlMetas['FaltaProgramar'] = sqlMetas['previsao']-(sqlMetas['estoque-saldoAnt'] + sqlMetas['carga'])
-    sqlMetas['FaltaProgramar'] = sqlMetas.apply(lambda l: l['FaltaProgramar']if l['FaltaProgramar'] >0 else 0 ,axis=1 )
+    sqlMetas['FaltaProgramar1'] = sqlMetas['previsao']-(sqlMetas['estoque-saldoAnt'] + sqlMetas['carga'])
+    sqlMetas['FaltaProgramar'] = sqlMetas.apply(lambda l: l['FaltaProgramar1']if l['FaltaProgramar1'] >0 else 0 ,axis=1 )
 
     sqlMetas.to_csv('./dados/analise.csv')
 
