3bc74c30d97ad3ea5e1e83dd139c5151e5b16727
luis-fe <luisengp@gmail.com>
2024-07-23 15:00:05 -0300

    api cronograma

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import gc
 from routes import routes_blueprint
 
 app = Flask(__name__)
-port = int(os.environ.get('PORT',5001))
+port = int(os.environ.get('PORT',8000))
 
 app.register_blueprint(routes_blueprint)
 
