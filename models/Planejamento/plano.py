3416af2708005ae788e2b2da0d308be758bf5a77
luis-fe <luisengp@gmail.com>
2024-07-24 11:03:55 -0300

    alteando

--- models/Planejamento/plano.py
+++ models/Planejamento/plano.py
@@ -46,8 +46,8 @@ def ObeterPlanos():
     # Agrupa mantendo todas as colunas do DataFrame planos e transforma lotes e nomelote em arrays
     grouped = merged.groupby(['01- Codigo Plano', '02- Descricao do Plano', '03- Inicio Venda', '04- Final Venda',
                               '05- Inicio Faturamento', '06- Final Faturamento', '07- Usuario Gerador', '08- Data Geracao']).agg({
-        'lote': lambda x: list(x.dropna().astype(str)),
-        'nomelote': lambda x: list(x.dropna().astype(str)),
+        'lote': lambda x: list(x.dropna().astype(str).unique()),
+        'nomelote': lambda x: list(x.dropna().astype(str).unique()),
         'tipoNota': lambda x: list(x.dropna().astype(str).unique())
     }).reset_index()
 
