74b6271c65db645d20b369a8f77d574fc8e7643c
luis-fe <luisengp@gmail.com>
2024-07-26 08:27:12 -0300

    realizado medida

--- models/GestaoOPAberto/justificativasOP.py
+++ models/GestaoOPAberto/justificativasOP.py
@@ -4,43 +4,47 @@ Arquivo para as justificativas no modulo Painel de Gestao da OP
 import pandas as pd
 from connection import ConexaoPostgreWms, ConexaoBanco
 
+
+'''Função de Consultar a Justificativa'''
 def ConsultarJustificativa(ordemProd, fase):
     conn = ConexaoPostgreWms.conexaoEngine()
-    conn2 = ConexaoBanco.Conexao2()
-
     consultaPostgre = 'SELECT ordemprod as "numeroOP", fase as "codFase", justificativa FROM "PCP".pcp.justificativa ' \
                 'WHERE ordemprod = %s and fase = %s '
-
     consultaPostgre = pd.read_sql(consultaPostgre,conn,params=(ordemProd, fase,))
 
 
-    consulta2 =pd.read_sql('SELECT CONVERT(varchar(12), codop) as numeroOP, codfase as codFase, textolinha as justificativa FROM tco.ObservacoesGiroFasesTexto  t '
+    conn2 = ConexaoBanco.Conexao2()
+    consultaCSW_justificativa =pd.read_sql('SELECT CONVERT(varchar(12), codop) as numeroOP, codfase as codFase, textolinha as justificativa FROM tco.ObservacoesGiroFasesTexto  t '
                                     'WHERE empresa = 1 and textolinha is not null',conn2)
-    consulta2['codFase'] = consulta2['codFase'].astype(str)
-
-    consulta2 = consulta2[consulta2['numeroOP'] == ordemProd]
-    consulta2 = consulta2[consulta2['codFase'] == str(fase)]
-
+    consultaCSW_justificativa['codFase'] = consultaCSW_justificativa['codFase'].astype(str)
+    consultaCSW_justificativa = consultaCSW_justificativa[consultaCSW_justificativa['numeroOP'] == ordemProd]
+    consultaCSW_justificativa = consultaCSW_justificativa[consultaCSW_justificativa['codFase'] == str(fase)]
     conn2.close()
 
-    if consulta2.empty and not consultaPostgre.empty:
+
+    # Caso nao tenha justificativa no CSW porém tenha justificativa no banco do PCP
+    if consultaCSW_justificativa.empty and not consultaPostgre.empty:
         consulta = consultaPostgre
-        print('teste1')
+        return consulta # Retorna Justificativa do Banco do PCP
 
-    elif consulta2.empty and consultaPostgre.empty:
+
+    # Caso nao tenha justiicativa em ambos os bancos
+    elif consultaCSW_justificativa.empty and consultaPostgre.empty:
         consulta = pd.DataFrame([{'justificativa': 'sem justificativa'}])
+        return consulta
 
-    elif not consulta2.empty and consultaPostgre.empty:
+    # Caso  tenha justificativa no CSW porém nao tenha justificativa no banco do PCP
+    elif not consultaCSW_justificativa.empty and consultaPostgre.empty:
+        consulta = consultaCSW_justificativa
+        return consulta
 
-        consulta = pd.DataFrame([{'justificativa': 'sem justificativa'}])
-
-
+    # Caso tenha justificativa em ambos os bancos , assume do banco do postgre
     else:
         consulta = consultaPostgre
+        return consulta
 
 
-    return consulta
-
+'''FUNCAO CADASTRAR A JUSTIFICATIVA '''
 def CadastrarJustificativa(ordemProd, fase , justificativa):
     fase = str(fase)
     consultar = ConsultarJustificativa(ordemProd, fase)
