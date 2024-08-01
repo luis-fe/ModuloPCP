eb87c2bb869cc0a88a96bca73cbb2adcfc82ae1b
luis-fe <luisengp@gmail.com>
2024-08-01 08:38:51 -0300

    testando o calculo

--- models/MonitorPedidos/AutomacaoOPs.py
+++ models/MonitorPedidos/AutomacaoOPs.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from connection import ConexaoBanco, ConexaoPostgreWms
+import numpy as np
 
 def PesquisandoReduzido():
     conn = ConexaoPostgreWms.conexaoEngine()
@@ -20,15 +21,16 @@ def PesquisandoReduzido():
 
 
 def BuscandoOPCSW(empresa):
-    with ConexaoBanco.ConexaoInternoMPL() as conn:  ##Abrindo Conexao Com o CSW
 
-        sqlCswOpsnivelSku = """
+    sqlCswOpsnivelSku = """
         SELECT op.codfaseatual ,ot.codProduto ,ot.numeroop as numeroop , ot.codSortimento , seqTamanho, 
         case WHEN ot.qtdePecas1Qualidade is null then ot.qtdePecasProgramadas else qtdePecas1Qualidade end total_pcs 
         FROM tco.OrdemProdTamanhos ot
         inner join tco.ordemprod op on op.codempresa = ot.codempresa and op.numeroop = ot.numeroop
         having ot.codEmpresa = """ + empresa + """ and ot.numeroOP IN """ + ' (select o.numeroOP  from tco.ordemprod o where o.situacao = 3 and o.codempresa = ' + empresa + ')'
 
+    sqlCsw_em_aberto2 = ' select o.numeroOP as numeroop,  o.codTipoOP, codSeqRoteiroAtual as seqAtual  from tco.ordemprod o where o.situacao = 3 and o.codempresa = ' + empresa
+    with ConexaoBanco.ConexaoInternoMPL() as conn:  ##Abrindo Conexao Com o CSW
         with conn.cursor() as cursor_csw:
             # Executa a primeira consulta e armazena os resultados
             cursor_csw.execute(sqlCswOpsnivelSku)
@@ -37,7 +39,6 @@ def BuscandoOPCSW(empresa):
             get = pd.DataFrame(rows, columns=colunas)
             del rows
 
-            sqlCsw_em_aberto2 = ' select o.numeroOP as numeroop,  o.codTipoOP, codSeqRoteiroAtual as seqAtual  from tco.ordemprod o where o.situacao = 3 and o.codempresa = ' + empresa
             cursor_csw.execute(sqlCsw_em_aberto2)
             colunas = [desc[0] for desc in cursor_csw.description]
             rows = cursor_csw.fetchall()
@@ -50,7 +51,11 @@ def BuscandoOPCSW(empresa):
             get['codSortimento'] = get['codSortimento'].astype(str)
             get['seqTamanho'] = get['seqTamanho'].astype(str)
             get = pd.merge(get, sku, on=["codProduto", "codSortimento", "seqTamanho"], how='left')
-            get['id'] = get.apply(lambda r: 9000 if r['codTipoOP'] in [1, 4] else 1000, axis=1)
+            # Atribui valores iniciais à coluna 'id' com base na coluna 'codTipoOP'
+            get['id'] = np.where(get['codTipoOP'].isin([1, 4]), 9000, 2000)
+            # Atualiza os valores da coluna 'id' com base na coluna 'codfaseatual'
+            get['id'] = np.where(get['codfaseatual'].isin([1, 401]), 1000, get['id'])
+
             get['id'] = get['id'] + get['seqAtual'].astype(int)
             get['id'] = get['id'].astype(str) + '||' + get['codreduzido'].astype(str)
             get = get.sort_values(by=['codreduzido', 'id'], ascending=False)  # escolher como deseja classificar
