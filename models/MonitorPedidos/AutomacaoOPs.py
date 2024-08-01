b5271656f3c86939850fcc145f2d338f30c414c5
luis-fe <luisengp@gmail.com>
2024-08-01 16:21:19 -0300

    testando o calculo

--- models/MonitorPedidos/AutomacaoOPs.py
+++ models/MonitorPedidos/AutomacaoOPs.py
@@ -64,7 +64,7 @@ def BuscandoOPCSW(empresa):
 
             get2 = get[(get['pesquisa']>1) & (get['codTipoOP']==1)]
             get2 = get2.sort_values(by=['codreduzido', 'numeroop'], ascending=True)  # escolher como deseja classificar
-            get2['NovoseqAtual'] = (get2.groupby('concatenar')['concatenar'].cumcount()+1)*0.1
+            get2['NovoseqAtual'] = 1-((get2.groupby('concatenar')['concatenar'].cumcount()+1)*0.1)
             get2['seqAtual'] = get2['seqAtual'].astype(int)
             get2['seqAtual'] = get2.groupby('concatenar')['seqAtual'].transform('max')
             get2['NovoseqAtual'] = get2['seqAtual'].astype(str) + get2['NovoseqAtual'].astype(str)
