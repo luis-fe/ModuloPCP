e58dc14302b53fe7449d235cc7e20471a99b7c58
luis-fe <luisengp@gmail.com>
2024-08-02 13:42:11 -0300

    filtro FiltrodataEmissaoInicial

--- models/GestaoOPAberto/FilaFases.py
+++ models/GestaoOPAberto/FilaFases.py
@@ -137,8 +137,8 @@ def ApresentacaoFila(COLECAO):
     if colecoes['COLECAO'][0] != '-':
         fila = pd.merge(fila, colecoes , on='COLECAO')
         start = pd.DataFrame(
-            {'numeroOP': ['t-0', 't-0'], 'codFase': [449, 437], "codFaseAtual": [449, 437], "Situacao": 'em processo',
-             "pcs": 0, 'COLECAO': ['', ''], "fase": ['ENTRADA DE ESTOQUE', 'ACABAMENTO EXTERNO']})
+            {'numeroOP': ['t-0', 't-0','t-0'], 'codFase': [449, 437,412], "codFaseAtual": [449, 437,412], "Situacao": ['em processo','em processo','a produzir'],
+             "pcs": 0, 'COLECAO': ['', ''], "fase": ['ENTRADA DE ESTOQUE', 'ACABAMENTO EXTERNO','PRODUCAO DE MEIAS']})
         fila = pd.concat([fila,start])
 
     fila.to_csv('./dados/filaroteiroOP.csv')
