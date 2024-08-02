15b971712b68e7672b01eadea323f67db6b600b4
luis-fe <luisengp@gmail.com>
2024-08-02 14:05:40 -0300

    filtro FiltrodataEmissaoInicial

--- models/GestaoOPAberto/FilaFases.py
+++ models/GestaoOPAberto/FilaFases.py
@@ -76,9 +76,9 @@ def FilaFases():
     fila['Situacao'] = np.where((fila['codFase'] == 426) & (fila['tipoOP'] == 2),
                                 'prouzido', fila['Situacao'])
     fila['Situacao'] = np.where((fila['codFase'] == 412) & (fila['tipoOP'] == 4),
-                                'prouzido', fila['Situacao'])
+                                'produzido', fila['Situacao'])
     fila['Situacao'] = np.where((fila['codFase'] == 441) & (fila['tipoOP'] == 4),
-                                'prouzido', fila['Situacao'])
+                                'produzido', fila['Situacao'])
 
 
     sql_nomeFases = """
