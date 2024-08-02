64c4f5eb9c353f479f387601a05088150d5793c7
luis-fe <luisengp@gmail.com>
2024-08-02 15:51:00 -0300

    filtro FiltrodataEmissaoInicial

--- models/Planejamento/SaldoPlanoAnterior.py
+++ models/Planejamento/SaldoPlanoAnterior.py
@@ -84,6 +84,12 @@ def Monitor_nivelSku(dataFim):
     # Filtrar as datas
     df_loaded['filtro'] = (df_loaded['dataEmissao'] <= data_datetime) & (df_loaded['dataEmissao'] >= data_inic)
 
+    # 2 - Filtrar Apenas Pedidos Não Bloqueados
+    pedidosBloqueados = Monitor_PedidosBloqueados()
+    df_loaded = pd.merge(df_loaded,pedidosBloqueados,on='codPedido',how='left')
+    df_loaded['situacaobloq'].fillna('Liberado',inplace=True)
+    df_loaded = df_loaded[df_loaded['situacaobloq'] == 'Liberado']
+
     # Aplicar o filtro
     df_filtered = df_loaded[df_loaded['filtro']].reset_index(drop=True)
 
@@ -146,3 +152,85 @@ def Pedidos_saldo(codigoPlano, codItem):
     pedidos.rename(columns={'saldo': 'saldoEntregar'}, inplace=True)
 
     return pedidos
+
+def FaturamentoPlano(codigoPlano):
+    planoAtual = plano.ConsultaPlano()
+    planoAtual = planoAtual[planoAtual['codigo']==codigoPlano].reset_index()
+
+    # Levantar as data de início e fim do faturamento:
+    IniFat = planoAtual['inicoFat'][0]
+    FimFat = planoAtual['finalFat'][0]
+
+    #Filtrando somente os pedidos
+
+
+
+
+
+
+def Monitor_PedidosBloqueados():
+    consultacsw = """SELECT * FROM (
+    SELECT top 300000 bc.codPedido, 'analise comercial' as situacaobloq  from ped.PedidoBloqComl  bc WHERE codEmpresa = 1  
+    and bc.situacaoBloq = 1
+    order by codPedido desc
+    UNION 
+    SELECT top 300000 codPedido, 'analise credito'as situacaobloq  FROM Cre.PedidoCreditoBloq WHERE Empresa  = 1  
+    and situacao = 1
+    order BY codPedido DESC) as D"""
+
+    with ConexaoBanco.Conexao2() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(consultacsw)
+            colunas = [desc[0] for desc in cursor.description]
+            rows = cursor.fetchall()
+            consulta = pd.DataFrame(rows, columns=colunas)
+
+        del rows
+        return consulta
+
+
+def Monitor_nivelSkuFaturamento(dataEmissaoIni, dataEmissaoFinal):
+    # Carregar o arquivo Parquet
+    parquet_file = fp.ParquetFile('./dados/pedidos.parquet')
+
+    # Converter para DataFrame do Pandas
+    df_loaded = parquet_file.to_pandas()
+
+    # Converter 'dataEmissao' para datetime
+    df_loaded['dataPrevFat'] = pd.to_datetime(df_loaded['dataPrevFat'], errors='coerce', infer_datetime_format=True)
+
+    # Convertendo a string para datetime
+    data_datetime = pd.to_datetime(dataEmissaoIni)
+    # Subtraindo 100 dias
+    data_inic = data_datetime - pd.DateOffset(days=100)
+
+    # Filtrar as datas
+    df_loaded['filtro'] = (df_loaded['dataPrevFat'] <= data_datetime) & (df_loaded['dataEmissao'] >= data_inic)
+
+    # 2 - Filtrar Apenas Pedidos Não Bloqueados
+    pedidosBloqueados = Monitor_PedidosBloqueados()
+    df_loaded = pd.merge(df_loaded, pedidosBloqueados, on='codPedido', how='left')
+    df_loaded['situacaobloq'].fillna('Liberado', inplace=True)
+    df_loaded = df_loaded[df_loaded['situacaobloq'] == 'Liberado']
+
+    # Aplicar o filtro
+    df_filtered = df_loaded[df_loaded['filtro']].reset_index(drop=True)
+
+    # Selecionar colunas relevantes
+    df_filtered = df_filtered.loc[:,
+                  ['codPedido', 'codProduto', 'qtdePedida', 'qtdeFaturada', 'qtdeCancelada', 'qtdeSugerida',
+                   'PrecoLiquido', 'codTipoNota']]
+
+    # Convertendo colunas para numérico
+    df_filtered['qtdeSugerida'] = pd.to_numeric(df_filtered['qtdeSugerida'], errors='coerce').fillna(0)
+    df_filtered['qtdePedida'] = pd.to_numeric(df_filtered['qtdePedida'], errors='coerce').fillna(0)
+    df_filtered['qtdeFaturada'] = pd.to_numeric(df_filtered['qtdeFaturada'], errors='coerce').fillna(0)
+    df_filtered['qtdeCancelada'] = pd.to_numeric(df_filtered['qtdeCancelada'], errors='coerce').fillna(0)
+
+    # Adicionando coluna 'codItem'
+    df_filtered['codItem'] = df_filtered['codProduto']
+
+    # Calculando saldo
+    df_filtered['saldo'] = df_filtered["qtdePedida"] - df_filtered["qtdeFaturada"] - df_filtered["qtdeCancelada"]
+
+    return df_filtered
