7a96651ba4516f989ba4a51f7335b11b0e0aec4f
luis-fe <luisengp@gmail.com>
2024-08-05 15:59:02 -0300

    implementando os faccionsitas

--- routes/MonitorPedidos/monitor_route.py
+++ routes/MonitorPedidos/monitor_route.py
@@ -30,7 +30,7 @@ def get_MonitorPedidos():
     FiltrodataEmissaoInicial = request.args.get('FiltrodataEmissaoInicial','')
     FiltrodataEmissaoFinal = request.args.get('FiltrodataEmissaoFinal','')
 
-    print(request)
+    print(FiltrodataEmissaoFinal)
     #controle.InserindoStatus(rotina, ip, datainicio)
     dados = monitor.API(empresa, iniVenda, finalVenda, tiponota,'rotina', 'ip', 'datainicio',parametroClassificacao, tipoData, arrayRepres_excluir, arrayRepre_Incluir, nomeCliente,FiltrodataEmissaoInicial ,FiltrodataEmissaoFinal)
     #controle.salvarStatus(rotina, ip, datainicio)
