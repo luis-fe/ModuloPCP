ab81d75218cf594f59781dc39bd3493d2f0213c5
luis-fe <luisengp@gmail.com>
2024-08-05 14:37:53 -0300

    implementando os faccionsitas

--- routes/Faccionista/faccionista.py
+++ routes/Faccionista/faccionista.py
@@ -32,4 +32,23 @@ def get_ConsultaFaccionistasCsws():
     del dados
     return jsonify(OP_data)
 
+@faccionista_routes.route('/pcp/api/ObterCategorias', methods=['GET'])
+@token_required
+def get_ObterCategorias():
+
+    dados = faccionistas.ObterCategorias()
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
+
+
 
