53fe7eaf3006aa0de98ae81d8b5a079721c11545
luis-fe <luisengp@gmail.com>
2024-08-06 11:07:44 -0300

    implementando os faccionsitas2

--- models/Planejamento/Meta_faccionistas.py
+++ models/Planejamento/Meta_faccionistas.py
@@ -11,11 +11,12 @@ def MetasFaccionistas(codigoPlano,arrayCodLoteCsw,dataMovFaseIni, dataMovFaseFim
     consulta1 = pd.read_sql(sql,conn,params=(codigoPlano,codLote,))
 
     #Passo2 carregando as capacidades dos faccionistas
-    sql = """select nomecategoria as categoria, codfaccionista, "Capacidade/dia" from "PCP".pcp."faccaoCategoria" fc  """
+    sql = """select nomecategoria as categoria, codfaccionista, "Capacidade/dia"::int from "PCP".pcp."faccaoCategoria" fc  """
     consulta2 = pd.read_sql(sql,conn)
 
     #Passo3 Realizando o merge
-    consulta1 = pd.merge(consulta1,consulta2,on='categoria')
-
+    consulta1 = pd.merge(consulta1,consulta2,on='categoria',how='left')
+    consulta1['Capacidade/dia'].fillna(0,inplace=True)
+    consulta1.fillna('-',inplace=True)
 
     return consulta1
