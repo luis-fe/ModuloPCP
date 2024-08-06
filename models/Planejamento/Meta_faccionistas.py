0f1ea83b1947a2c0271e959a75ca4076e6445f14
luis-fe <luisengp@gmail.com>
2024-08-06 11:24:14 -0300

    implementando os faccionsitas2

--- models/Planejamento/Meta_faccionistas.py
+++ models/Planejamento/Meta_faccionistas.py
@@ -19,10 +19,10 @@ def MetasFaccionistas(codigoPlano,arrayCodLoteCsw,dataMovFaseIni, dataMovFaseFim
     consulta1['Capacidade/dia'].fillna(0,inplace=True)
     consulta1.fillna('-',inplace=True)
 
-    consulta1['capacidadeSoma'] = consulta1.groupby('categoria')['Capacidade/dia'].sum()
+    consulta1['capacidadeSoma'] = consulta1.groupby('categoria')['Capacidade/dia'].transform('sum')
     consulta1['exedente'] = consulta1['Meta Dia'] - consulta1['capacidadeSoma']
     consulta1_ = consulta1[consulta1['exedente']>0]
     consulta1_ = consulta1_.groupby('categoria').agg({'exedente':'first'}).reset_index()
 
 
-    return consulta1
+    return consulta1_
