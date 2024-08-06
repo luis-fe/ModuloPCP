c165a81f4539ceeba1fce5b65e271cab33aed4bf
luis-fe <luisengp@gmail.com>
2024-08-06 11:06:01 -0300

    implementando os faccionsitas2

---
+++ models/Planejamento/Meta_faccionistas.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from connection import ConexaoPostgreWms
+
+
+def MetasFaccionistas(codigoPlano,arrayCodLoteCsw,dataMovFaseIni, dataMovFaseFim, congelado):
+    # passo 1 Carregar o plano informado
+    conn = ConexaoPostgreWms.conexaoEngine()
+    sql = """select * from "backup"."metaCategoria" where "plano" = %s and "codLote" = %s """
+
+    codLote = arrayCodLoteCsw[0]
+    consulta1 = pd.read_sql(sql,conn,params=(codigoPlano,codLote,))
+
+    #Passo2 carregando as capacidades dos faccionistas
+    sql = """select nomecategoria as categoria, codfaccionista, "Capacidade/dia" from "PCP".pcp."faccaoCategoria" fc  """
+    consulta2 = pd.read_sql(sql,conn)
+
+    #Passo3 Realizando o merge
+    consulta1 = pd.merge(consulta1,consulta2,on='categoria')
+
+
+    return consulta1
