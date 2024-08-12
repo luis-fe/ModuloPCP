fe4eb4bd6a9ca93c761baea72e974a7d2ea09f7f
luis-fe <luisengp@gmail.com>
2024-08-12 12:43:50 -0300

    necessidade de materiais

--- models/NecessidadesDeMP/necessidadeMP.py
+++ models/NecessidadesDeMP/necessidadeMP.py
@@ -1,8 +1,10 @@
 import gc
-
+import pytz
+import numpy as np
 import pandas as pd
 from connection import ConexaoPostgreWms, ConexaoBanco
-
+from models.Planejamento import SaldoPlanoAnterior, itemsPA_Csw, plano
+from datetime import datetime
 
 def AnaliseDeMateriais(codPlano, codLote, congelado):
     conn = ConexaoPostgreWms.conexaoEngine()
@@ -16,10 +18,62 @@ def AnaliseDeMateriais(codPlano, codLote, congelado):
         """
         sqlMetas = pd.read_sql(sqlMetas,conn, params=(codLote,))
 
+        consulta = """
+               select codigo as "codItem", nome, "unidadeMedida" , "codItemPai" , "codSortimento" as "codSortimento" , "codSeqTamanho" as "codSeqTamanho"  from pcp.itens_csw ic 
+               """
+        consulta = pd.read_sql(consulta, conn)
+        # Verificar quais codItemPai começam com '1' ou '2'
+        mask = consulta['codItemPai'].str.startswith(('1', '2'))
+        # Aplicar as transformações usando a máscara
+        consulta['codEngenharia'] = np.where(mask, '0' + consulta['codItemPai'] + '-0', consulta['codItemPai'] + '-0')
+
+        sqlMetas = pd.merge(sqlMetas, consulta, on=["codEngenharia", "codSeqTamanho", "codSortimento"], how='left')
+        sqlMetas['codItem'].fillna('-', inplace=True)
+        saldo = SaldoPlanoAnterior.SaldosAnterior(codPlano)
+        sqlMetas = pd.merge(sqlMetas, saldo, on='codItem', how='left')
+
+        faturado = SaldoPlanoAnterior.FaturamentoPlano(codPlano)
+        sqlMetas = pd.merge(sqlMetas, faturado, on='codItem', how='left')
+
+        estoque, cargas = itemsPA_Csw.EstoquePartes()
+        sqlMetas = pd.merge(sqlMetas, estoque, on='codItem', how='left')
+
+        # cargas = itemsPA_Csw.CargaFases()
+        sqlMetas = pd.merge(sqlMetas, cargas, on='codItem', how='left')
+
+        sqlMetas.fillna({
+            'saldo': 0,
+            'qtdeFaturada': 0,
+            'estoqueAtual': 0,
+            'carga': 0
+        }, inplace=True)
+
+        # Analisando se esta no periodo de faturamento
+        diaAtual = datetime.strptime(obterDiaAtual(), '%Y-%m-%d')
+
+        planoAtual = plano.ConsultaPlano()
+        planoAtual = planoAtual[planoAtual['codigo'] == codPlano].reset_index()
+
+        # Levantar as data de início e fim do faturamento:
+        IniFat = datetime.strptime(planoAtual['inicoFat'][0], '%Y-%m-%d')
+
+        if diaAtual >= IniFat:
+            sqlMetas['FaltaProgramar1'] = sqlMetas['previsao'] - (
+                        sqlMetas['estoqueAtual'] + sqlMetas['carga'] + sqlMetas['qtdeFaturada'])
+        else:
+            sqlMetas['estoque-saldoAnt'] = sqlMetas['estoqueAtual'] - sqlMetas['saldo']
+            sqlMetas['FaltaProgramar1'] = sqlMetas['previsao'] - (sqlMetas['estoque-saldoAnt'] + sqlMetas['carga'])
+        try:
+            sqlMetas['FaltaProgramar'] = sqlMetas.apply(
+                lambda l: l['FaltaProgramar1'] if l['FaltaProgramar1'] > 0 else 0, axis=1)
+        except:
+            print('verificar')
+
         #Obtendo os consumos de todos os componentes relacionados nas engenharias
         sqlcsw = """
         SELECT v.codProduto as codEngenharia, cv.codSortimento, cv.seqTamanho as codSeqTamanho,  v.CodComponente,
         (SELECT i.nome FROM cgi.Item i WHERE i.codigo = v.CodComponente) as descricaoComponente,
+        (SELECT i.unidadeMedida FROM cgi.Item i WHERE i.codigo = v.CodComponente) as unid,
         cv.quantidade  from tcp.ComponentesVariaveis v 
         join tcp.CompVarSorGraTam cv on cv.codEmpresa = v.codEmpresa and cv.codProduto = v.codProduto and cv.sequencia = v.codSequencia 
         WHERE v.codEmpresa = 1
@@ -27,6 +81,7 @@ def AnaliseDeMateriais(codPlano, codLote, congelado):
         UNION 
         SELECT v.codProduto as codEngenharia,  l.codSortimento ,l.codSeqTamanho as codSeqTamanho, v.CodComponente,
         (SELECT i.nome FROM cgi.Item i WHERE  i.codigo = v.CodComponente) as descricaoComponente,
+        (SELECT i.unidadeMedida FROM cgi.Item i WHERE i.codigo = v.CodComponente) as unid,
         v.quantidade  from tcp.ComponentesPadroes  v 
         join tcl.LoteSeqTamanho l on l.Empresa = v.codEmpresa and l.codEngenharia = v.codProduto and l.codlote = '"""+codLote+"""'"""
 
@@ -41,10 +96,16 @@ def AnaliseDeMateriais(codPlano, codLote, congelado):
         del rows
         gc.collect()
 
-        sqlMetas = pd.merge(sqlMetas, consumo, on=["codEngenharia" , "codSeqTamanho" , "codSortimento"], how='left')
-        sqlMetas['quantidade'].fillna(0,inplace=True)
-        sqlMetas['quantidade Prevista'] = sqlMetas['quantidade'] * sqlMetas['previsao']
-        sqlMetas = sqlMetas.groupby(["CodComponente"]).agg({"descricaoComponente":"first","quantidade Prevista":"sum"}).reset_index()
+        Necessidade = pd.merge(sqlMetas, consumo, on=["codEngenharia" , "codSeqTamanho" , "codSortimento"], how='left')
+        Necessidade['quantidade'].fillna(0,inplace=True)
+        Necessidade['quantidade Prevista'] = Necessidade['quantidade'] * Necessidade['previsao']
+        Necessidade["FaltaProgramarMeta"] = Necessidade['quantidade'] * Necessidade['FaltaProgramar']
+        Necessidade = Necessidade.groupby(["CodComponente"]).agg({"descricaoComponente":"first","quantidade Prevista":"sum","FaltaProgramarMeta":"sum"}).reset_index()
 
 
-        return sqlMetas
+        return Necessidade
+def obterDiaAtual():
+    fuso_horario = pytz.timezone('America/Sao_Paulo')  # Define o fuso horário do Brasil
+    agora = datetime.now(fuso_horario)
+    agora = agora.strftime('%Y-%m-%d')
+    return agora
