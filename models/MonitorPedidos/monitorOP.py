c8ddf5cff8b4e97918357f48d2d904780cdebbf8
luis-fe <luisengp@gmail.com>
2024-08-13 18:12:54 -0300

    RealizadoGeralCostura

--- models/MonitorPedidos/monitorOP.py
+++ models/MonitorPedidos/monitorOP.py
@@ -448,7 +448,7 @@ def ProdutosSemOP_(dataInico, dataFim):
 
     monitorDetalhadoOps = monitorDetalhadoOps.sort_values(by=['QtdSaldo'],
                                       ascending=[False]).reset_index()
-    monitorDetalhadoOps.rename(columns={'codEngenharia': 'codItemPai'}, inplace=True)
+    monitorDetalhadoOps.rename(columns={'codItemPai': 'codEngenharia'}, inplace=True)
     monitorDetalhadoOps['codEngenharia'] = monitorDetalhadoOps['codEngenharia'].astype(str)
     monitorDetalhadoOps['codEngenharia'] = '0'+monitorDetalhadoOps['codEngenharia']+'-0'
 
