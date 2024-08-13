9578691b103aa0f778379978cc42a7881a3ee8e1
luis-fe <luisengp@gmail.com>
2024-08-13 10:00:03 -0300

    necessidade de materiais

--- models/Planejamento/acomp_meta_plano.py
+++ models/Planejamento/acomp_meta_plano.py
@@ -380,7 +380,7 @@ def MetasCostura(Codplano, arrayCodLoteCsw, dataMovFaseIni, dataMovFaseFim, cong
 
         codLote = arrayCodLoteCsw[0]
         Meta = pd.read_sql(sql, conn, params=(Codplano, codLote,))
-        Meta.drop(['Realizado','apresentacao','dias','nomeFase','codLote'], axis=1, inplace=True)
+        Meta.drop(['Realizado','codLote'], axis=1, inplace=True)
 
 
         realizado = realizadoFases.RealizadoFaseCategoria(dataMovFaseIni, dataMovFaseFim,429)
