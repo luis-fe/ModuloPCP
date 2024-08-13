0056735b68e46281fb9a003cf41c0a7a1aa6b799
luis-fe <luisengp@gmail.com>
2024-08-13 10:11:44 -0300

    necessidade de materiais

--- models/GestaoOPAberto/realizadoFases.py
+++ models/GestaoOPAberto/realizadoFases.py
@@ -448,3 +448,40 @@ def ObterCategorias():
     consulta = pd.read_sql(sql,conn)
 
     return consulta
+
+
+def RealizadoFaseDia(dataMovFaseIni,dataMovFaseFim,codFase):
+    CarregarRealizado(60)
+    sql = """
+        select rf."codEngenharia",
+    	rf.numeroop ,
+    	rf.codfase:: varchar as "codFase", rf."seqRoteiro" , rf."dataBaixa"::date , rf."nomeFaccionista", rf."codFaccionista" , rf."horaMov"::time,
+    	rf."totPecasOPBaixadas" as "Realizado", rf."descOperMov" as operador, rf.chave 
+    from
+    	"PCP".pcp.realizado_fase rf 
+    where 
+    	rf."dataBaixa"::date >= %s 
+    	and rf."dataBaixa"::date <= %s ;
+        """
+
+    conn = ConexaoPostgreWms.conexaoEngine()
+    realizado = pd.read_sql(sql, conn, params=(dataMovFaseIni, dataMovFaseFim,))
+
+    realizado['codFase'] = np.where(realizado['codFase'].isin(['431', '455', '459']), '429', realizado['codFase'])
+    realizado = realizado[realizado['codFase']==str(codFase)].reset_index()
+
+    conn = ConexaoPostgreWms.conexaoEngine()
+    sqlNomeEngenharia = """
+    select ic."codItemPai"::varchar , max(ic.nome)::varchar as nome from "PCP".pcp.itens_csw ic where ("codItemPai" like '1%') or ("codItemPai" like '5%') group by "codItemPai"
+    """
+    NomeEngenharia = pd.read_sql(sqlNomeEngenharia,conn)
+
+    NomeEngenharia['codEngenharia'] = NomeEngenharia.apply(
+        lambda r: '0' + r['codItemPai'] + '-0' if r['codItemPai'].startswith('1') else r['codItemPai'] + '-0', axis=1)
+    realizado = pd.merge(realizado,NomeEngenharia,on='codEngenharia',how='left')
+    realizado['categoria'] = '-'
+    realizado['nome'] = realizado['nome'].astype(str)
+    realizado['categoria'] = realizado['nome'].apply(mapear_categoria)
+    realizado = realizado.groupby(["codFase","dataBaixa"]).agg({"Realizado":"sum"}).reset_index()
+
+    return realizado
