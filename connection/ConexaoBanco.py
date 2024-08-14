3e27d039160092a0f0ed122dc445681884c3bf65
luis-fe <luisengp@gmail.com>
2024-08-14 11:10:17 -0300

    RealizadoGeralCostura

--- connection/ConexaoBanco.py
+++ connection/ConexaoBanco.py
@@ -2,15 +2,20 @@ import jaydebeapi
 import gc
 from contextlib import contextmanager
 
+@contextmanager
 def Conexao2():
-    conn = jaydebeapi.connect(
-    'com.intersys.jdbc.CacheDriver',
-    'jdbc:Cache://187.32.10.129:1972/CONSISTEM',
-    {'user': 'root', 'password': 'ccscache'},
-    './connection/CacheDB.jar'
-)
-    gc.collect()
-    return conn
+    conn = None
+    try:
+        conn = jaydebeapi.connect(
+            'com.intersys.jdbc.CacheDriver',
+            'jdbc:Cache://192.168.0.25:1972/CONSISTEM',
+            {'user': 'root', 'password': 'ccscache'},
+            './connection/CacheDB.jar'
+        )
+        yield conn
+    finally:
+        if conn:
+            conn.close()
 
 @contextmanager
 def ConexaoInternoMPL():
