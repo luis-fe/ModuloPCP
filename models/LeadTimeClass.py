5c62784353dc9bccd8fc05070108af0753583235
luis-fe <luisengp@gmail.com>
2024-09-10 14:40:07 -0300

    atualizando regra de calculo do faccionista

--- models/LeadTimeClass.py
+++ models/LeadTimeClass.py
@@ -346,6 +346,7 @@ class LeadTimeCalculator:
             realizado = pd.merge(realizado, codtipoops, on=['codtipoop'])
 
         if self.categorias != []:
+            print(self.categorias)
             categorias = pd.DataFrame(self.categorias, columns=["categoria"])
             realizado = pd.merge(realizado, categorias, on=['categoria'])
 
