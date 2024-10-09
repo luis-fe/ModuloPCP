bc794d832cea896f3ad8857c02307e1d9bc1bd01
luis-fe <luisengp@gmail.com>
2024-10-09 17:32:46 -0300

    acrescentando o caminho absoluto no codigo

--- models/StatusOpsEmProcesso.py
+++ models/StatusOpsEmProcesso.py
@@ -111,6 +111,7 @@ class StatusOpsEmProcesso():
         if self.nomeFaccionista == None:
 
             carga = pd.merge(consultaCategoriaFacc, consultarOPCsw, on=['codfaccionista', 'categoria'], how='right')
+            carga['leadtime'] =carga['leadtime'].fillna(0,inplace=True)
 
             carga['dataPrevista'] = pd.to_datetime(carga['dataEnvio'])
 
