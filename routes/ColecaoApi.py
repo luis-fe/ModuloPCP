07f86c91f585b1ed77a314cd024d7012f4c0914e
luis-fe <luisengp@gmail.com>
2024-10-26 15:52:07 -0300

    atualizacao3

--- routes/ColecaoApi.py
+++ routes/ColecaoApi.py
@@ -64,3 +64,25 @@ def post_VincularColecoesPlano():
         OP_data.append(op_dict)
     del dados
     return jsonify(OP_data)
+
+
+
+@ColecaoAPI_routes.route('/pcp/api/ConsultaLotesVinculados', methods=['GET'])
+@token_required
+def get_ConsultaLotesVinculados():
+    codPlano = request.args.get('codPlano')
+
+    dados = colec.Colecao(None,codPlano).obterColecoesporPlano()
+    #controle.salvarStatus(rotina, ip, datainicio)
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
