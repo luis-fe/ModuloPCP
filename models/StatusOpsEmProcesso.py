0c6e37e343fd9f1053cf2d256b3f8902b7234815
luis-fe <luisengp@gmail.com>
2024-10-25 21:03:56 -0300

    atualizacao

--- models/StatusOpsEmProcesso.py
+++ models/StatusOpsEmProcesso.py
@@ -579,8 +579,13 @@ class StatusOpsEmProcesso():
                 resumoStatus = resumoStatus.groupby(['status']).agg(
                     {'carga': 'sum'}).reset_index()
 
-            resumoStatus = resumoStatus.groupby(['status']).agg(
-                {'carga': 'sum'}).reset_index()
+
+            if (self.nomeFaccionista == None or self.nomeFaccionista == '') and (self.nomecategoria == None and self.nomecategoria == ''):
+
+                resumoStatus = resumoStatus1.groupby(['status']).agg(
+                    {'carga': 'sum'}).reset_index()
+
+
             consulta['carga'].fillna(0, inplace=True)
             consulta = consulta[consulta['carga'] > 0].reset_index()
             consulta.fillna("-", inplace=True)
