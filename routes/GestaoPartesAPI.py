d4bae950c601bc5de95e50c7890a6cea47adb62a
luis-fe <luisengp@gmail.com>
2024-10-30 20:50:00 -0300

    Estrutura partes

--- routes/GestaoPartesAPI.py
+++ routes/GestaoPartesAPI.py
@@ -110,6 +110,23 @@ def get_EstoqueFuturoNat20():
     del dados
     return jsonify(OP_data)
 
+@GestaoPartes_routes.route('/pcp/api/EstuturaPartes', methods=['GET'])
+def get_EstuturaPartes():
+
+    dados = GP.GestaoPartes('','426','425').estruturaItensPartes()
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
+
 
 
 
