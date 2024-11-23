5bb08c4de0585b575c162f88db3aadb448594119
luis-fe <luisengp@gmail.com>
2024-11-23 17:13:40 -0300

    atualizando o crors

--- models/EstoqueSkuClass.py
+++ models/EstoqueSkuClass.py
@@ -36,19 +36,7 @@ sum("total_pcs") as "estoqueAtual",0 as estReservPedido
 from
 	"pcp".ordemprod o
 where
-	"qtdAcumulada" > 0 and ("codFaseAtual" in ('441' ,'466','437') or "numeroop" in (
-	'138771-001',
-'138875-001',
-'138958-001',
-'139470-001',
-'140578-001',
-'140588-001',
-'140612-001',
-'140623-001',
-'140783-001',
-'140968-001',
-'141031-001'
-	))
+	"qtdAcumulada" > 0 and ("codFaseAtual" in ('441' ,'466','437', '432','465') )
 group by codreduzido
         """
 
