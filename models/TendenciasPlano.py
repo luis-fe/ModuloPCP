571dcb1a9287b8c8673d70d62a9bbf5718dc0dee
luis-fe <luisengp@gmail.com>
2024-12-23 08:56:36 -0300

    parametroABC

--- models/TendenciasPlano.py
+++ models/TendenciasPlano.py
@@ -4,19 +4,108 @@ from connection import ConexaoPostgreWms
 class TendenciaPlano():
     '''Classe utilizada para a analise de tendencias de vendas de um determinado Plano '''
 
-    def __init__(self, codPlano = None, parametroABC = None):
+    def __init__(self, codPlano = None, parametroABC = None, perc_dist = None):
 
         self.codPlano = codPlano
         self.parametroABC = parametroABC
+        self.perc_dist = perc_dist
 
     def consultaPlanejamentoABC(self):
         '''Metodo utilizado para planejar a distribuicacao ABC'''
+
+        sql = """
+        Select "nomeABC" , "perc_dist" from "Plano_ABC"
+        where 
+            "codPlano" = %s
+        order by 
+            "nomeABC"
+        """
+
+        conn = ConexaoPostgreWms.conexaoEngine()
+        consulta = pd.read_sql(sql, conn, params=(self.codPlano,))
+
+        totalDistribuido = consulta['perc_dist'].sum()
+        faltaDistribuir = 100 - totalDistribuido
+
+        data = {
+                '1- Total Distribuido': f'{totalDistribuido}%',
+                '2- Falta Distribuir':f'{faltaDistribuir}%',
+                '3- Detalhamento:': consulta.to_dict(orient='records')
+            }
+        return pd.DataFrame([data])
+
+
+    def inserirPlanejamentoABC(self):
+        '''Metodo utilizado para inserir plano'''
+
+        inserir = """
+        insert into "Plano_ABC" ("codPlano", "nomeABC", "perc_dist") values ( %s, %s, %s)
+        """
+
+        with ConexaoPostgreWms.conexaoInsercao() as conn:
+            with conn.cursor() as curr:
+
+                curr.execute(inserir,(self.codPlano, self.parametroABC,self.perc_dist))
+                conn.commit()
+
+        return pd.DataFrame([{'status':True,'Mensagem':'Planejamento ABC alterado com sucesso'}])
+
+    def updatePlanejamentoABC(self):
+        '''Metodo que faz o update no planejamento ABC do Plano'''
+
+        update = """
+        update "Plano_ABC"
+        set "perc_dist" = %s
+        where "nomeABC" = %s and "codPlano" = %s
+        """
+
+        with ConexaoPostgreWms.conexaoInsercao() as conn:
+            with conn.cursor() as curr:
+
+                curr.execute(update,(self.perc_dist, self.parametroABC,self.codPlano))
+                conn.commit()
+
+        return pd.DataFrame([{'status':True,'Mensagem':'Planejamento ABC alterado com sucesso'}])
+
+    def inserirOuAlterarPlanj_ABC(self):
+        '''Metodo para inserir ou alterar o planejamento abc '''
+        verifica1 = self.consultaParametrizacaoABC()
+        verifica1 = verifica1[verifica1['nomeABC'] == self.parametroABC].reset_index()
+
+        if verifica1.empty:
+            return pd.DataFram([{'status': False, 'Mensagem': 'Parametro Abc nao existe !'}])
+
+        verifica = self.consultaPlanejamentoABC()
+        if not verifica.empty:
+            self.inserirPlanejamentoABC()
+        else:
+            self.updatePlanejamentoABC()
+        return pd.DataFram([{'status':True,'Mensagem':'Inserido ou Atualizado com sucesso!'}])
+
+    def consultaPlanABC(self):
+
+        sql = """"
+        select * from "Plano_ABC" 
+        where "nomeABC" = %s and "codPlano" = %s
+        """
+        conn = ConexaoPostgreWms.conexaoEngine()
+        consulta = pd.read_sql(sql,conn,params=(self.parametroABC, self.codPlano,))
+        return consulta
+
+
+    def arrayinserirOuAlterarPlanj_ABC(self, arrayABC , arrayPercentual):
+        '''Metodo para inserir via abc '''
+
+
+
+
 
     def consultaParametrizacaoABC(self):
         '''Metodo utilizado para consultar a parametrizacao do ABC'''
 
         sql = """
         select "nomeABC" from pcp."parametroABC"
+        order by "nomeABC" asc
         """
         conn = ConexaoPostgreWms.conexaoEngine()
         consulta = pd.read_sql(sql,conn)
--- routes/TendenciaApi.py
+++ routes/TendenciaApi.py
@@ -60,3 +60,22 @@ def post_CadastrarParametroABC():
     del dados
     return jsonify(OP_data)
 
+@tendencia_routes.route('/pcp/api/consultaPlanejamentoABC_plano', methods=['GET'])
+@token_required
+def get_consultaPlanejamentoABC_plano():
+
+    codPlano = request.args.get('codPlano','-')
+    dados = TendenciasPlano.TendenciaPlano(codPlano).consultaPlanejamentoABC()
+    #controle.salvarStatus(rotina, ip, datainicio)
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
