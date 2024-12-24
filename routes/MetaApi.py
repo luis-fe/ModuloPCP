897cca8bed797b27f63a442bced6850bfefe3640
luis-fe <luisengp@gmail.com>
2024-12-24 09:18:12 -0300

    consultarMetaCategoriaPlano

--- routes/MetaApi.py
+++ routes/MetaApi.py
@@ -122,3 +122,30 @@ def get_consultarMetaCategoriaPlano():
         OP_data.append(op_dict)
     del dados
     return jsonify(OP_data)
+
+
+@metas_routes.route('/pcp/api/atualizaOuInserirMetaCategoria', methods=['POST'])
+@token_required
+def post_atualizaOuInserirMetaCategoria():
+    data = request.get_json()
+
+    codPlano = data.get('codPlano')
+    marca = data.get('marca', '-')
+    metaFinanceira = data.get('metaFinanceira', '-')
+    metaPecas = data.get('metaPecas', '-')
+    nomeCategoria = data.get('nomeCategoria', '-')
+
+    dados = Meta.Meta(codPlano,marca, metaFinanceira, metaPecas, nomeCategoria).atualizaOuInserirMetaCategoria()
+    #controle.salvarStatus(rotina, ip, datainicio)
+
+    # Obtém os nomes das colunas
+    column_names = dados.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    OP_data = []
+    for index, row in dados.iterrows():
+        op_dict = {}
+        for column_name in column_names:
+            op_dict[column_name] = row[column_name]
+        OP_data.append(op_dict)
+    del dados
+    return jsonify(OP_data)
