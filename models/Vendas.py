8481f1d04a086b90d25eac383aa8d7d356e1ceee
luis-fe <luisengp@gmail.com>
2024-12-24 12:35:30 -0300

    VendasPlanoSKU

--- models/Vendas.py
+++ models/Vendas.py
@@ -55,7 +55,7 @@ class VendasAcom():
         if 'level_0' in df_loaded.columns:
             df_loaded = df_loaded.drop(columns=['level_0'])
         df_loaded = df_loaded[df_loaded['filtro3'] == True].reset_index()
-        #df_loaded = df_loaded[df_loaded['filtro4'] == True].reset_index()
+        df_loaded = df_loaded[df_loaded['filtro4'] == True].reset_index()
 
         df_loaded = df_loaded.loc[:,
                     ['codPedido', 'codProduto', 'qtdePedida', 'qtdeFaturada', 'qtdeCancelada', 'qtdeSugerida','codTipoNota',
