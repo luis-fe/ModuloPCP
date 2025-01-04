8ba2cccc9a6bb2fa7ce9bc6a5871773fb42b641e
luis-fe <luisengp@gmail.com>
2025-01-04 15:19:54 -0300

    abc

--- models/AnaliseMateriais.py
+++ models/AnaliseMateriais.py
@@ -286,7 +286,7 @@ class AnaliseMateriais():
         df_loaded['codItemPai'].fillna('-', inplace=True)
         df_loaded = df_loaded.loc[:,
                          ['codItemPai']]
-        df_loaded['codItemPai'] = df_loaded['codItemPai'].drop_duplicates()
+        df_loaded['codItemPai'] = df_loaded['codItemPai'].where(~df_loaded['codItemPai'].duplicated())
 
         print(df_loaded)
 
