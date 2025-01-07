6ca400e324765a951a54409220927d4b2f35b028
luis-fe <luisengp@gmail.com>
2025-01-07 18:53:23 -0300

    abc

--- models/AnaliseMateriais.py
+++ models/AnaliseMateriais.py
@@ -360,5 +360,6 @@ class AnaliseMateriais():
         consumo['LeadTime'].fillna(1,inplace=True)
         consumo['CodComponente'] = consumo['CodComponente'].astype(str)
         consumo['CodComponente'] = consumo['CodComponente'].str.replace('.0','')
+        print(consumo[consumo['CodComponente'] == '1831'])
 
         return consumo
