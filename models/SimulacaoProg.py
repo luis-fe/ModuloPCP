7492206c434c17b72a14415248c2ae175a1646fb
luis-fe <luisengp@gmail.com>
2025-01-16 16:34:29 -0300

    atualizacao 16_01-2025/2: inserindo a ideia de cadastro de simulacao: cadastro de simulacao

--- models/SimulacaoProg.py
+++ models/SimulacaoProg.py
@@ -240,6 +240,7 @@ class SimulacaoProg():
 
         for _, row in AbcDataFrame.iterrows():  # O índice é descartado com '_'
             self.classAbc = row['class']  # Acessa diretamente o valor da coluna
+            self.perc_abc = row['percentual']  # Acessa diretamente o valor da coluna
             self.inserirAbcSimulacao()
 
         return pd.DataFrame([{'Mensagem':'Simulacao inserida ou alterada com sucesso','satus':True}])
