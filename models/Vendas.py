845371f942148cd9fac4b837fdf2df21d9c800cc
luis-fe <luisengp@gmail.com>
2025-01-20 12:41:50 -0300

    atualizacao 18_01-2025/1: crod simulacao 1, acrescentando regra para nao distribuir quando bater a meta !

--- models/Vendas.py
+++ models/Vendas.py
@@ -261,7 +261,6 @@ class VendasAcom():
         groupBy = groupBy.sort_values(by=['qtdePedida'],
                                                         ascending=False)  # escolher como deseja classificar
         tam = ProdutosClass.Produto().get_tamanhos()
-        groupBy['codSeqTamanho'] = groupBy['codSeqTamanho'].astype(str).str.replace('.0','')
         tam['codSeqTamanho'] = tam['codSeqTamanho'].astype(str).str.replace('.0','')
         groupBy = pd.merge(groupBy,tam,on='codSeqTamanho',how='left')
 
