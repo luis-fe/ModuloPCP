f075724749ac8c5a8dd0ca3c03fdb9b6c51d3795
luis-fe <luisengp@gmail.com>
2025-02-26 08:50:43 -0300

    atualizando o agrupamento tipo de op

--- routes/routesModels/LeadTimeRoutes.py
+++ routes/routesModels/LeadTimeRoutes.py
@@ -65,6 +65,7 @@ def get_ObterTipoOP():
 def get_tiposDeProducaoAgrupado():
 
     dados = TipoOPClass.TipoOP().tiposDeProducaoAgrupado()
+    print(dados)
 
     # Converte o DataFrame para uma lista de dicionários de forma eficiente
     OP_data = dados.to_dict('records')
